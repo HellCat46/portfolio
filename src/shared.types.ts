839e751ff81576a1ba8c3b38dacec3ee058f49ca
HellCat46 <hellcat2388@gmail.com>
2023-12-09 16:05:23 +0530

    Added More Tags

--- src/shared.types.ts
+++ src/shared.types.ts
@@ -1,8 +1,16 @@
 export const TAGS = {
+  // Languages
   Typescript: { name: "TS/JS", color: "#007acc" },
   Nodejs: { name: "Node", color: "#3c873a" },
   Rust: { name: "Rust", color: "#e43716" },
   C: { name: "C/C++", color: "#03589b" },
+  CSharp: { name: "C#", color: "#9d72d9" },
+  // Databases
+  MySQL: { name: "MySQL", color: "#e48e00" },
+  MsSQL: { name: "MsSQL", color: "#df2f29" },
+  Postgres: { name: "PostgreSQL", color: "#2f6792" },
+  Azure: { name: "Azure", color: "#06529a" },
+  Docker: { name: "Docker", color: "#0092e6" },
 } as const;
 export type Tags = (typeof TAGS)[keyof typeof TAGS];
 
