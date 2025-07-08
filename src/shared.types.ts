da7105bea8a8bf86cc1a11b7f762edc66c4d6622
HellCat46 <hellcat2388@gmail.com>
2025-07-08 13:01:07 +0530

    Updates Project Page

--- src/shared.types.ts
+++ src/shared.types.ts
@@ -4,8 +4,18 @@ export const TAGS = {
   Nodejs: { name: "Node", color: "#3c873a" },
   Rust: { name: "Rust", color: "#e43716" },
   C: { name: "C", color: "#03589b" },
+  CPP: { name: "C++", color: "#00599c" },
   CSharp: { name: "C#", color: "#9d72d9" },
   Java: { name: "Java", color: "#f8981d" },
+  Golang : { name: "Go", color: "#00a9e0" },
+  
+  // Libraries & Frameworks
+  Wayland: { name: "Wayland", color: "#ffbd00" },
+  Hyprland: { name: "Hyprland", color: "#00d4da" },
+  OpenSSL: { name: "OpenSSL", color: "#f0f0f0" },
+  Socket : { name : "Unix Socket", color: "#b4befe"},
+  Base64: { name: "Base64", color: "#f0f0f0" },
+  
   // Databases
   MySQL: { name: "MySQL", color: "#e48e00" },
   MsSQL: { name: "MsSQL", color: "#df2f29" },
